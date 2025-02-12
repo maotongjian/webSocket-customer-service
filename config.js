e9cf399ba47a3ce68ae4045f03383cb5b0415fc4
MTJ <17279760014@163.com>
2025-02-12 17:25:43 +0800

    【EDIT】修改服务器地址

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 8080,
   nodeEnv: process.env.NODE_ENV || "development",
-  wsUrl: process.env.WS_URL || "ws://localhost:8080",
+  wsUrl: process.env.WS_URL || "wss://web-production-e802f.up.railway.app",
 };
